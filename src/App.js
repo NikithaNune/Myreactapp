45bed635eedfde61f442ce5571a68391af762c41
Nikitha Nune <nunenikitha7132@gmail.com>
2024-05-28 23:13:23 +0530

    firt commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>   
+      <h1 class="text-danger text-center" >hii</h1>
+    <h2>hello world</h2></div>
+
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,5 @@
+function Home(){
+    return(
+        <div><h1>hii</h1></div>
+    )
+}
